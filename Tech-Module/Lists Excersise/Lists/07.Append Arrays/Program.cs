4a1095fce1ee8ce786b029562e2e3be75e969f4e
SvetlinNikolov <daxteranikolov@gmai.com>
2019-03-12 20:05:21 +0200

    Update

---
+++ Tech-Module/Lists Excersise/Lists/07.Append Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07.Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputArray = Console.ReadLine().Split('|').Reverse()
+                .ToList();
+
+            var numbers = new List<int>();
+
+            foreach (var str in inputArray)
+            {
+                numbers.AddRange(str.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse));
+            }
+            Console.WriteLine(string.Join(" ",numbers));
+
+        }
+    }
+}
