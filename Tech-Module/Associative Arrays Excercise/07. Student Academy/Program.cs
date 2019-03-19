8d976cb8c80c7c57a3ec96b9889cc391df10baa9
SvetlinNikolov <daxteranikolov@gmai.com>
2019-03-19 19:24:26 +0200

    Uploading Array Exercise

---
+++ Tech-Module/Associative Arrays Excercise/07. Student Academy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _07._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            var studentAndGradeDict = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+
+                if (!studentAndGradeDict.ContainsKey(studentName))
+                {
+                    studentAndGradeDict.Add(studentName, new List<double>());
+                    studentAndGradeDict[studentName].Add(studentGrade);
+                }
+                else
+                {
+                    studentAndGradeDict[studentName].Add(studentGrade);
+                }
+            }
+            var betterStudents = studentAndGradeDict.Where(j => j.Value.Average() >= 4.50);
+
+            foreach (var student in betterStudents
+                .OrderByDescending(x => x.Value.Average()))
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+            }
+        }
+    }
+}
