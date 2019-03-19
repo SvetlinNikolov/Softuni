58c90ae8384485fe9daa48951c7d0f1e4d32439a
SvetlinNikolov <daxteranikolov@gmai.com>
2019-03-19 19:23:00 +0200

    Uploading Array Exercise

---
+++ Tech-Module/Associative_Arrays_Excercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _01.Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var dictionary = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (dictionary.ContainsKey(input[i]))
+                {
+
+                    dictionary[input[i]]++;
+                }
+                else
+                {
+                    dictionary.Add(input[i], 1);
+                }
+            }
+            foreach (var word in dictionary)
+            {
+                if (word.Key != ' ')
+                {
+                    Console.WriteLine($"{word.Key} -> {word.Value}");
+                }
+
+            }
+        }
+    }
+
+}
---
+++ Tech-Module/Associative_Arrays_Excercise/Associative_Arrays_Excercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Associative_Arrays_Excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
