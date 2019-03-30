4e92600f5ce3bdab6b2a5398059d8b8d733e6fcd
SvetlinNikolov <daxteranikolov@gmail.com>
2019-03-30 22:03:14 +0200

    Added Final Exam

---
+++ Tech-Module/Final Exam 2018/Final Exam 2018/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Final_Exam_2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
