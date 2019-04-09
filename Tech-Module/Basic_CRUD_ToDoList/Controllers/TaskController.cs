e47a1923e366dc38ee367dab6417d97090280cd7
SvetlinNikolov <daxteranikolov@gmail.com>
2019-04-09 17:44:45 +0300

    Updating

---
+++ Tech-Module/Basic_CRUD_ToDoList/Controllers/TaskController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Data;
+using ToDoList.Models;
+using Task = ToDoList.Models.Task;
+
+namespace ToDoList.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (var db = new ToDoDbContext())
+            {
+                var allTasks = db.Tasks.ToList();
+                return View(allTasks);
+            }
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string title, string comments)
+        {
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(comments))
+            {
+                return RedirectToAction("Index");
+            }
+            Task task = new Task
+            {
+                Title = title,
+                Comments = comments
+            };
+            using (var db = new ToDoDbContext())
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
