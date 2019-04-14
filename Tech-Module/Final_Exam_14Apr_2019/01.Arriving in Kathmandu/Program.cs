e0b561c51338381e8d274617b52d717761494916
SvetlinNikolov <daxteranikolov@gmail.com>
2019-04-14 13:03:53 +0300

    Uploading Final Exam 2019

---
+++ Tech-Module/Final_Exam_14Apr_2019/01.Arriving in Kathmandu/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string regexPattern = @"(^[!@#$?A-z0-9!@#$?]+)=([0-9]+)<<(.+)";
+            Regex fullRegex = new Regex(regexPattern);
+
+            string nameOfmountain = string.Empty;
+
+            while (input != "Last note")
+            {
+                if (fullRegex.IsMatch(input))
+                {
+                    Match match = fullRegex.Match(input);
+
+                    string[] coordinates = input.Split("<<").ToArray();
+                    string[] mountainNameArray = input.Split("=").ToArray();
+                    string mountainName = mountainNameArray[0];
+
+                    int coordinatesLenght = coordinates.Last().Length;
+                    string coordinatesOfMountain = coordinates.Last();
+
+                    int lenghtOfMountainGroupInRegex = int.Parse(match.Groups[2].Value);
+
+                    if (coordinatesLenght == lenghtOfMountainGroupInRegex)
+                    {
+                        foreach (var symbol in mountainName)
+                        {
+                            if (char.IsLetterOrDigit(symbol))
+                            {
+                                nameOfmountain += symbol;
+                            }
+                        }
+                        Console.WriteLine($"Coordinates found! { nameOfmountain} -> { coordinatesOfMountain}");
+                        nameOfmountain = string.Empty;
+                        input = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                        input = Console.ReadLine();
+                        nameOfmountain = string.Empty;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                    input = Console.ReadLine();
+                    nameOfmountain = string.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ Tech-Module/Final_Exam_14Apr_2019/02. On the Way to Annapurna/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace _02.On_the_Way_to_Annapurna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string initialInput = Console.ReadLine();
+            string[] input = initialInput.Split("->").ToArray();
+
+            string item = string.Empty;
+            var dictionary = new Dictionary<string, List<string>>();
+
+            while (initialInput != "END")
+            {
+                string command = input[0];
+                string store = input[1];
+
+                if (input.Length > 2)
+                {
+                    item = input[2];
+                }
+
+                if (command == "Add" && !item.Contains(','))
+                {
+                    if (!dictionary.ContainsKey(store))
+                    {
+                        dictionary.Add(store, new List<string>());
+                        dictionary[store].Add(item);
+                    }
+                    else if (dictionary.ContainsKey(store))
+                    {
+                        dictionary[store].Add(item);
+                    }
+                }
+                else if (command == "Add")
+                {
+                    if (!dictionary.ContainsKey(store))
+                    {
+                        dictionary.Add(store, new List<string>());
+
+                        string[] items = input[2].Split(',').ToArray();
+
+                        for (int i = 0; i < items.Length; i++)
+                        {
+                            dictionary[store].Add(items[i]);
+                        }
+                    }
+                    else if (dictionary.ContainsKey(store))
+                    {
+                        string[] items = input[2].Split(',').ToArray();
+
+                        for (int i = 0; i < items.Length; i++)
+                        {
+                            dictionary[store].Add(items[i]);
+                        }
+
+                    }
+                }
+                else if (command == "Remove")
+                {
+                    if (dictionary.ContainsKey(store))
+                    {
+                        dictionary.Remove(store);
+                    }
+                }
+
+                initialInput = Console.ReadLine();
+                input = initialInput.Split("->").ToArray();
+            }
+            Console.WriteLine("Stores list:");
+
+            foreach (var store in dictionary.OrderByDescending(x => x.Value.Count).ThenByDescending(x => x.Key))
+            {
+                Console.WriteLine(store.Key);
+                foreach (var items in store.Value)
+                {
+                    Console.WriteLine($"<<{items}>>");
+                }
+            }
+        }
+    }
+}
