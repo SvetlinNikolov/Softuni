df9e44b08738005ae4622a5e10e32c47e8265218
SvetlinNikolov <daxteranikolov@gmail.com>
2019-05-27 22:02:42 +0300

    Uploading Streams and Directories Lab

---
+++ C# Advanced/Streams_And_File_Directories/01. Odd Lines/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace Streams_And_File_Directories
+{
+    using System;
+    using System.IO;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"D:\african\Text.txt"))
+            {
+                using (var writer = new StreamWriter(@"D:\african\Kekst.txt"))
+
+                {
+                    int counter = 0;
+
+                    while (true)
+                    {
+
+
+                        string currentLine = reader.ReadLine();
+
+                        if (currentLine == null)
+                        {
+                            break;
+                        }
+                        else
+                        {
+
+
+                            if (counter % 2 != 0)
+                            {
+                                writer.WriteLine(currentLine);
+                            }
+
+                            counter++;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Streams_And_File_Directories/02. Line Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace _02._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            using(var reader = new StreamReader("Text.txt"))
+            {
+                using(var writer = new StreamWriter("Kekst.txt"))
+                {
+                    string currentLine = reader.ReadLine();
+                    int counter = 1;
+                    while (currentLine != null)
+                    {
+                       
+                        writer.WriteLine($"{counter}. {currentLine}");
+                        counter++;
+                        currentLine = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
