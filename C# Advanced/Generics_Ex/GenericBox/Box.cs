060f9c00c07547c95c40ac5c2fe16ba94f93affc
SvetlinNikolov <daxteranikolov@gmail.com>
2019-06-13 21:50:46 +0300

    Uploading Generic Ex 1

---
+++ C# Advanced/Generics_Ex/GenericBox/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBox
+{
+    public class Box<T>
+    {
+        private List<T> boxValues;
+
+        public Box()
+        {
+            boxValues = new List<T>();
+        }
+        public void Add(T item)
+        {
+            boxValues.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in boxValues)
+            {
+                sb.AppendLine($"{item.GetType().FullName}: {item}");
+            }
+
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ C# Advanced/Generics_Ex/GenericBox/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericBox
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputLines = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < inputLines; i++)
+            {
+             
+                box.Add(Console.ReadLine());
+            }
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
