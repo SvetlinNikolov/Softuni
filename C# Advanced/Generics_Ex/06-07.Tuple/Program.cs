e06f85876352f09df4226017ed93aabd5f8e3556
SvetlinNikolov <daxteranikolov@gmail.com>
2019-06-13 23:11:56 +0300

    Uploading 01-08 Generics Exercise

---
+++ C# Advanced/Generics_Ex/06-07.Tuple/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string[] nameAndAddress = Console.ReadLine()
+                .Split(" ");
+
+            string firstAndLastName = $"{nameAndAddress[0]} {nameAndAddress[1]}";
+            string address = nameAndAddress[2];
+            string town = nameAndAddress[3];
+
+            string[] nameAndLitersOfBeer = Console.ReadLine()
+                .Split(" ");
+
+            string nameOfPersonDrinkingBeer = nameAndLitersOfBeer[0];
+            int litersOfBeer = int.Parse(nameAndLitersOfBeer[1]);
+            bool drunkOrNot = nameAndLitersOfBeer[2] == "drunk" ? true : false;
+
+            string[] integerAndDouble = Console.ReadLine()
+                .Split(" ");
+
+            string personNameBankBalance = integerAndDouble[0];
+            double accountBalance = double.Parse(integerAndDouble[1]);
+            string bankName = integerAndDouble[2];
+
+
+            Tuple<string, string, string> nameAndAddressTuple =
+                new Tuple<string, string, string>(firstAndLastName, address, town);
+
+            Tuple<string, int, bool> nameAndLitersOfBeerTuple =
+                new Tuple<string, int, bool>(nameOfPersonDrinkingBeer, litersOfBeer, drunkOrNot);
+
+            Tuple<string, double, string> intAndDoubleTuple =
+                new Tuple<string, double, string>(personNameBankBalance, accountBalance, bankName);
+
+
+            Console.WriteLine(nameAndAddressTuple.GetInfo());
+            Console.WriteLine(nameAndLitersOfBeerTuple.GetInfo());
+            Console.WriteLine(intAndDoubleTuple.GetInfo());
+        }
+    }
+}
---
+++ C# Advanced/Generics_Ex/06-07.Tuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    public class Tuple<T, V, W>
+    {
+        public T item1 { get; set; }
+        public V item2 { get; set; }
+        public W item3 { get; set; }
+        public Tuple(T item1, V item2, W item3)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+        }
+        public string GetInfo()
+        {
+            return $"{this.item1} -> {this.item2} -> {this.item3}";
+        }
+
+
+    }
+}
