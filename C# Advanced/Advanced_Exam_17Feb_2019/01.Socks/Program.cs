34f0f94da2647035ce5523999259b37462ae3757
SvetlinNikolov <daxteranikolov@gmail.com>
2019-06-15 18:54:58 +0300

    Uploading c# advanced exam excercise

---
+++ C# Advanced/Advanced_Exam_17Feb_2019/01.Socks/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advanced_Exam_17Feb_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] leftSide = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int[] rightSide = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Stack<int> leftSideSocks = new Stack<int>(leftSide);
+            Queue<int> rightSideSocks = new Queue<int>(rightSide);
+
+            List<int> allSetsOfSocks = new List<int>();
+
+            for (int i = 0; i < Math.Max(leftSide.Length, rightSide.Length); i++)
+            {
+                if (leftSideSocks.Count == 0 || rightSideSocks.Count == 0)
+                {
+                    break;
+                }
+
+                if (leftSideSocks.Peek() > rightSideSocks.Peek())
+                {
+                    int pairValue = leftSideSocks.Pop() + rightSideSocks.Dequeue();
+                    allSetsOfSocks.Add(pairValue);
+                }
+                else if (rightSideSocks.Peek() > leftSideSocks.Peek())
+                {
+                    while (leftSideSocks.Count > 0 && rightSideSocks.Count > 0 && rightSideSocks.Peek() > leftSideSocks.Peek())
+                    {
+                        leftSideSocks.Pop();
+                    }
+                }
+                else if (leftSideSocks.Peek() == rightSideSocks.Peek())
+                {
+                    rightSideSocks.Dequeue();
+                    leftSideSocks.Push(leftSideSocks.Pop() + 1);
+
+                }
+            }
+            Console.WriteLine(allSetsOfSocks.Max());
+            Console.WriteLine(string.Join(" ", allSetsOfSocks));
+        }
+    }
+}
---
+++ C# Advanced/Advanced_Exam_17Feb_2019/02.Excel Functions/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02.Excel_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixDimensions = int.Parse(Console.ReadLine());
+
+            string[] header = Console.ReadLine()
+                .Split(", ");
+
+            string[,] matrix = new string[matrixDimensions - 1, header.Length];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] elementsOfMatrix = Console.ReadLine()
+                        .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elementsOfMatrix[col];
+
+                }
+            }
+
+            string[] command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string currentCommand = command[0];
+            string currentHeader = command[1];
+
+            int indexOfHeaderElement = Array.IndexOf(header, currentHeader);
+            if (currentCommand == "hide")
+            {
+                for (int rows = 0; rows < matrix.GetLength(0); rows++)
+                {
+                    matrix[rows, indexOfHeaderElement] = null;
+                }
+            }
+            else if (currentCommand == "sort")
+            {
+                Array.Sort(matrix);
+            }
+
+
+            Console.WriteLine(string.Join(" | ", header.Where(x => x != currentHeader)));
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] != null)
+                    {
+                        if (col == matrix.GetLength(1) - 1)
+                        {
+                            Console.Write(matrix[row, col]);
+                        }
+                        else
+                        {
+                            Console.Write(matrix[row, col] + " | ");
+                        }
+
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
