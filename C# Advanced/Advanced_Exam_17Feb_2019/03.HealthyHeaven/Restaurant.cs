35bee415fc45e26393cf55e34fff8a2c1655f9ca
SvetlinNikolov <daxteranikolov@gmail.com>
2019-06-17 20:04:42 +0300

    Uploading 03 of exam

---
+++ C# Advanced/Advanced_Exam_17Feb_2019/03.HealthyHeaven/Restaurant.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HealthyHeaven
+{
+    public class Restaurant
+    {
+        private List<Salad> saladData;
+
+        public string Name { get; set; }
+
+        public Restaurant(string name)
+        {
+            this.Name = name;
+            saladData = new List<Salad>();
+        }
+
+        public void Add(Salad salad)
+        {
+            saladData.Add(salad);
+        }
+
+
+        public bool Buy(string name)
+        {
+            if (saladData.Exists(x => x.Name == name))
+            {
+                Salad saladToRemove = saladData.Where(x => x.Name == name).ToList()[0];
+                saladData.Remove(saladToRemove);
+                return true;
+            }
+            
+            return false;
+        }
+
+        public string GetHealthiestSalad()
+        {
+            Salad healthiestSalad = new Salad("shhh");
+
+            foreach (var salad in saladData)
+            {
+                if (healthiestSalad.GetTotalCalories() == 0)
+                {
+                    healthiestSalad = salad;
+                }
+                if (salad.GetTotalCalories() < healthiestSalad.GetTotalCalories())
+                {
+                    healthiestSalad = salad;
+                }
+            }
+            return healthiestSalad.Name;
+
+        }
+
+        public string GenerateMenu()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string saladInfo = $"{Name} have {saladData.Count} salads:";
+
+            sb.AppendLine(saladInfo);
+
+            foreach (var salad in saladData)
+            {
+                sb.AppendLine(salad.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# Advanced/Advanced_Exam_17Feb_2019/03.HealthyHeaven/Salad.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthyHeaven
+{
+    public class Salad
+    {
+        private List<Vegetable> saladCollection;
+
+        public string Name { get; set; }
+
+        public Salad(string name)
+        {
+            this.Name = name;
+            saladCollection = new List<Vegetable>();
+        }
+        public int GetTotalCalories()
+        {
+            int totalCalories = 0;
+
+            foreach (var vegetable in saladCollection)
+            {
+                totalCalories += vegetable.Calories;
+            }
+            return totalCalories;
+        }
+
+        public int GetProductCount()
+        {
+            return saladCollection.Count;
+        }
+
+        public void Add(Vegetable vegetable)
+        {
+            saladCollection.Add(vegetable);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string saladInformation =$"* Salad {Name} is {GetTotalCalories()} calories and have {GetProductCount()} products:";
+
+            sb.AppendLine(saladInformation);
+
+            foreach (var vegetable in saladCollection)
+            {
+                sb.AppendLine(vegetable.ToString());
+            }
+
+            return sb.ToString();
+           
+
+        }
+    }
+}
