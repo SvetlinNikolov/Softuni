89aabb6d271e7a2976977745c966c6df9d484f3f
SvetlinNikolov <daxteranikolov@gmail.com>
2019-07-05 23:30:20 +0300

    Update

---
+++ C# OOP/Inheritance_Ex/05.Restaurant/Products/Foods/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant.Products.Foods
+{
+    public class Cake : Dessert
+    {
+        public const double Grams = 250;
+        public const double Calories = 1000;
+        public const decimal CakePrice = 5M;
+
+        public Cake(string name, decimal price, double grams, double calories)
+            : base(name, CakePrice, Grams, Calories)
+        {
+        }
+    }
+}
---
+++ C# OOP/Inheritance_Ex/05.Restaurant/Products/Foods/Fish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Fish : MainDish
+    {
+        public const int Grams = 22;
+
+        public Fish(string name, decimal price, double grams)
+            : base(name, price, Grams)
+        {
+           
+        }
+    }
+}
