56f9950163c509617a19ba711f1bdf2273f0133a
SvetlinNikolov <daxteranikolov@gmail.com>
2019-07-10 16:56:11 +0300

    Uploading Abstraction and Interfaces Lab

---
+++ C# OOP/Interfaces_and_Abstraction_Lab/01. Shapes/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int height;
+        private int width;
+
+
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.Width, '*', '*');
+
+            for (int i = 1; i < this.height - 1; ++i)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+            DrawLine(this.width, '*', '*');
+        }
+
+        public void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+
+            for (int i = 1; i < width - 1; ++i)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
---
+++ C# OOP/Interfaces_and_Abstraction_Lab/02. Cars/Tesla.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : Seat, ICar, IElectricCar
+    {
+
+        public Tesla(string model, string color, int batteries)
+            : base(model, color)
+        {
+            this.Battery = batteries;
+        }
+
+        public int Battery { get; protected set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model} {this.Battery} Batteries")
+           .AppendLine(Start())
+           .AppendLine(Stop());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
