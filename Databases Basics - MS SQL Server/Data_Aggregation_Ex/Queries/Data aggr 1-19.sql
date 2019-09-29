1c1082cd52a6b62dcf8417b9f5db70d64229bdd9
SvetlinNikolov <daxteranikolov@gmail.com>
2019-09-29 14:57:00 +0300

    Update

--- Databases Basics - MS SQL Server/Data_Aggregation_Ex/Queries/Data aggr 1-19.sql
+++ Databases Basics - MS SQL Server/Data_Aggregation_Ex/Queries/Data aggr 1-19.sql
@@ -19,7 +19,12 @@ FROM WizzardDeposits
 GROUP BY DepositGroup
 
 --4
---TODO
+SELECT DepositGroup FROM (
+SELECT TOP (2) DepositGroup,  AVG(MagicWandSize) AS [AVERAGE SIZE]
+FROM WizzardDeposits
+GROUP BY DepositGroup
+ORDER BY [AVERAGE SIZE] 
+) AS [AvgSizeTable]
 
 --5
 SELECT DepositGroup, SUM(e.DepositAmount) AS TotalSum
@@ -71,16 +76,18 @@ ELSE '[61+]'
 END
 
 --10
-SELECT LEFT(FirstName,1) AS FirstLetter
+SELECT LEFT(FirstName, 1) AS FirstLetter
 FROM WizzardDeposits AS e
 WHERE e.DepositGroup = 'Troll Chest'
-GROUP BY LEFT(FirstName,1)
+GROUP BY LEFT(FirstName, 1)
 
 --11
---SELECT DepositGroup, IsDepositExpired, DepositInterest AS AverageInterest
---FROM WizzardDeposits
---GROUP BY DepositGroup
---HAVING IsDepositExpired 
+SELECT DepositGroup, IsDepositExpired , AVG(DepositInterest) AS [AverageInterest]
+FROM WizzardDeposits
+WHERE DepositStartdate > '1985/01/01'
+GROUP BY DepositGroup, IsDepositExpired
+ORDER BY DepositGroup DESC, IsDepositExpired ASC
+
 
 --12
 SELECT SUM([Difference]) AS SumDIfference FROM (SELECT FirstName AS [Host Wizard],
@@ -90,6 +97,42 @@ LEAD(DepositAmount) OVER (ORDER BY Id) AS [Guest Wizard Deposit],
 DepositAmount - LEAD(DepositAmount) OVER (ORDER BY Id) AS [Difference]
 FROM WizzardDeposits) AS DiffTable
 
+--13
+USE SoftUni
+
+SELECT DepartmentID, SUM(Salary) AS [TotalSalary]
+FROM Employees AS e
+GROUP BY DepartmentID
+ORDER BY DepartmentID ASC
+
+--14
+SELECT DepartmentID, MIN(Salary) AS [MinimumSalary]
+FROM Employees
+WHERE DepartmentID IN (2,5,7) AND HireDate > '2000/01/01'
+GROUP BY DepartmentID
+
+--15
+SELECT * INTO NewTable 
+FROM Employees AS e
+	WHERE e.Salary > 30000
+DELETE FROM NewTable WHERE ManagerID = 42
+UPDATE NewTable
+	SET Salary+=5000
+	WHERE DepartmentID = 1
+SELECT DepartmentID, AVG(Salary) AS [AverageSalary]
+FROM NewTable
+GROUP BY DepartmentID
+
+--16
+SELECT DepartmentID, MAX(Salary) AS [MaxSalary]
+FROM Employees
+GROUP BY DepartmentID
+HAVING MAX(Salary) NOT BETWEEN 30000 AND 70000
+
+--17
+SELECT Count(EmployeeID) AS [Count]
+FROM Employees
+WHERE ManagerID IS NULL
 --18
 SELECT DepartmentId, Salary AS [ThirdHighestSalary]
 FROM (SELECT DepartmentId, Salary, DENSE_RANK () OVER (PARTITION BY DepartmentId ORDER BY Salary DESC) AS [Ranking]
