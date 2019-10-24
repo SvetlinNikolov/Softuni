178a0b9a7a36f2a664904ca154652d71d4dc5c57
daxtera <daxteranikolov@gmail.com>
2019-10-24 23:36:04 +0300

    Update

---
+++ Entity Framework Core/Introduction_to_DB_Apps_Ex/05.ChangeTownNamesCasing/StartUp.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace _05.ChangeTownNamesCasing
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = (@"Server = DESKTOP-MU23NJN\SQLEXPRESS;
+                                              Database = MinionsDB;Integrated Security=true;");
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            sqlConnection.Open();
+
+            string country = Console.ReadLine();
+
+            SqlCommand findCountryExistsSql =
+                new SqlCommand($"SELECT c.Id FROM Countries AS c WHERE c.Name = '{country}'", sqlConnection);
+
+            int? countryId = (int?)findCountryExistsSql.ExecuteScalar();
+
+            if (countryId == null)
+            {
+                Console.WriteLine("No town names were affected.");
+                Environment.Exit(0);
+            }
+
+            SqlCommand townsInCountry =
+                new SqlCommand(@$"SELECT t.Name
+                FROM Towns as t
+                JOIN Countries AS c ON c.Id = t.CountryCode
+                WHERE c.Name = '{country}'", sqlConnection);
+
+            using (sqlConnection)
+            {
+
+                StringBuilder result = new StringBuilder();
+
+                SqlCommand updateTowns =
+                    new SqlCommand($@"
+                                UPDATE Towns
+                                SET Name = UPPER(Name)
+                                WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = '{country}')", sqlConnection);
+
+                int townsAffected = Convert.ToInt32(updateTowns.ExecuteNonQuery());
+
+                result.AppendLine($"{townsAffected} town names were affected.");
+
+                if (townsAffected == 0)
+                {
+                    Console.WriteLine("No town names were affected.");
+                    Environment.Exit(0);
+                }
+
+                SqlDataReader reader = townsInCountry.ExecuteReader();
+
+                result.Append("[");
+
+                int counter = 0;
+                while (reader.Read())
+                {
+                    counter++;
+
+                    if (counter < townsAffected)
+                    {
+                     
+                        result.Append(reader[0] + ", ");
+                    }
+                    else
+                    {
+                        result.Append(reader[0]);
+                    }
+
+
+                }
+                result.Append("]");
+
+                Console.WriteLine(result);
+                Environment.Exit(0);
+
+            }
+        }
+    }
+}
---
+++ Entity Framework Core/Introduction_to_DB_Apps_Ex/06.RemoveVillain/StartUp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _06.RemoveVillain
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = (@"Server = DESKTOP-MU23NJN\SQLEXPRESS;
+                                              Database = MinionsDB;Integrated Security=true;");
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            sqlConnection.Open();
+
+            int villainId = int.Parse(Console.ReadLine());
+
+            SqlCommand findVillainSql = new SqlCommand($"SELECT Name FROM Villains WHERE Id = '{villainId}'", sqlConnection);
+
+            string villainName = (string)findVillainSql.ExecuteScalar();
+
+            if (villainName == null)
+            {
+                Console.WriteLine("No such villain was found.");
+                Environment.Exit(0);
+            }
+
+            using (sqlConnection)
+            {
+                SqlTransaction transaction = sqlConnection.BeginTransaction();
+
+                try
+                {
+                     SqlCommand villainExistsSql = new SqlCommand($@"SELECT TOP(1) Name FROM Villains
+                                                              WHERE Id = '{villainId}'", sqlConnection, transaction);
+
+                    SqlCommand deleteVillain = new SqlCommand($@"DELETE FROM Villains
+                                                              WHERE Id = '{villainId}'", sqlConnection, transaction);
+
+                    int villainExists = Convert.ToInt32(villainExistsSql.ExecuteNonQuery());
+
+                    if (villainExists == 0)
+                    {
+                        throw new ArgumentException("No such villain was found.");
+                    }
+                   
+
+                    SqlCommand deleteMinionsFromVillain = new SqlCommand(@$"DELETE FROM MinionsVillains 
+                                                                         WHERE VillainId = '{villainId}'", sqlConnection, transaction);
+
+                    int minionsCount = Convert.ToInt32(deleteMinionsFromVillain.ExecuteNonQuery());
+                    deleteVillain.ExecuteNonQuery();
+
+                    transaction.Commit();
+
+                    Console.WriteLine($"{villainName} was deleted.");
+                    Console.WriteLine($"{minionsCount} minions were released.");
+                    
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+        }
+    }
+}
