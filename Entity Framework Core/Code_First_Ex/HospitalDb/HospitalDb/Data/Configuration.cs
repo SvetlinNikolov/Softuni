56ebf98f21dee8231e61b0426f53e970cdfe243d
daxtera <daxteranikolov@gmail.com>
2019-11-02 22:34:18 +0200

    Uploading hospital db

---
+++ Entity Framework Core/Code_First_Ex/HospitalDb/HospitalDb/Data/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data
+{
+   internal static class Configuration
+    {
+        internal static string ConnectionString =
+            @"Server = DESKTOP-MU23NJN\SQLEXPRESS;Database = SalesDatabase; Integrated Security = true";
+    }
+}
