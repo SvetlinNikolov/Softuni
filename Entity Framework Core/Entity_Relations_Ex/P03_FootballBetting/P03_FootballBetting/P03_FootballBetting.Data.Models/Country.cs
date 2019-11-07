e8e04e65c01956300114ff54e4123ec5e36567a3
daxtera <daxteranikolov@gmail.com>
2019-11-07 08:48:58 +0200

    uploading p3 football wip

---
+++ Entity Framework Core/Entity_Relations_Ex/P03_FootballBetting/P03_FootballBetting/P03_FootballBetting.Data.Models/Country.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+    }
+}
