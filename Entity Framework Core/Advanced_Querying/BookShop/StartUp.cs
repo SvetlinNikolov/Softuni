3353825955982e9c38a687e80a53d71e35e1811f
svetlin nikolov <daxteranikolov@gmail.com>
2019-11-08 17:16:14 +0200

    Uploading advanced querying ex wip

---
+++ Entity Framework Core/Advanced_Querying/BookShop/StartUp.cs
@@ -0,0 +1,79 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+                string result = GetGoldenBooks(db);
+                Console.WriteLine(result);
+            }
+
+   
+        }
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+
+            StringBuilder resultTitles = new StringBuilder();
+
+            TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+
+            command = ti.ToTitleCase(command);
+
+            var bookTitles = context
+                .Books
+                .Select(c => new
+                {
+                    BookTitle = c.Title,
+                    BookAgeRestriction = c.AgeRestriction
+                })
+                .Where(c => c.BookAgeRestriction.ToString() == command)
+                .OrderBy(c => c.BookTitle.ToString())
+                .ToList();
+
+
+            foreach (var bookTitle in bookTitles)
+            {
+                resultTitles.AppendLine(bookTitle.BookTitle);
+            }
+
+            return resultTitles.ToString().TrimEnd();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var goldenEditionBooks = context
+                .Books
+                .Select(e => new
+                {
+                    GoldenBookTitle = e.Title,
+                    BookCopies = e.Copies,
+                    EditionType = e.EditionType,
+                    BookId = e.BookId
+                })
+                .Where(e => e.EditionType.ToString().ToLower() == "gold"
+                && e.BookCopies < 5000)
+                .OrderBy(e => e.BookId)
+                .ToList();
+
+            foreach (var book in goldenEditionBooks)
+            {
+                sb.AppendLine(book.GoldenBookTitle.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
