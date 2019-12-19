d98bfb611e98cab3b30721fa5c28b5e6eaf3b653
daxtera <daxteranikolov@gmail.com>
2019-12-19 23:50:18 +0200

    This is a bit unstrucktured but it will only stay here untill I start C# web

---
+++ C# Web/Panda/Panda.Data/DbSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panda.Data
+{
+    public static class DbSettings
+    {
+        public const string ConnectionString = (@"Server=DESKTOP-MU23NJN\SQLEXPRESS;Database=Panda;Integrated Security=True");
+    }
+}
